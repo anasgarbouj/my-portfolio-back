1d03ef5fabec1b4d95a0bcebdd63756e320614f8
anas garbouj <anaskping@gmail.com>
2024-05-19 05:25:43 +0100

    adding loc data

--- middlewares/captureUserDetails.js
+++ middlewares/captureUserDetails.js
@@ -1,6 +1,21 @@
 const SessionDetails = require('../models/UserDetails'); // Ensure this path matches where you've saved your model
 const requestIp = require('request-ip');
 const useragent = require('useragent');
+
+const getLocationData = async (ip) => {
+    try {
+        const response = await axios.get(`https://ipinfo.io/${ip}/geo`);
+        return {
+            city: response.data.city,
+            region: response.data.region,
+            country: response.data.country
+        };
+    } catch (error) {
+        console.error('Failed to get location data', error);
+        return {};
+    }
+};
+
 const captureSessionDetails = async (req, res, next) => {
     const ip = requestIp.getClientIp(req);
     const ua = useragent.parse(req.headers['user-agent']);
